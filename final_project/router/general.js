6bed5e3b385b2a6275c71a20eecc512c6ede0d4a
SetWai <sw.setwai@gmail.com>
2026-02-16 02:02:33 +0400

    complete task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,7 +62,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   let isbn = req.params.isbn;
   res.send(books[isbn]);
- });
+});
+
+// Get book details based on ISBN using Promise callbacks
+public_users.get('/books/promise/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error fetching book details", error: error.message });
+    });
+});
+
+// Get book details based on ISBN using async-await
+public_users.get('/books/async/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn;
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching book details", error: error.message });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
